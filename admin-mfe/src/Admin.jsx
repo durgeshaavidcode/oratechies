c816f9d8417044566f5b1316ebb7224463270324
Durgesh <durgesh.yadav@fyndna.com>
2026-06-11 20:07:46 +0530

    testing error

--- admin-mfe/src/Admin.jsx
+++ admin-mfe/src/Admin.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import App from './App.jsx';
-import { store } from './store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { store } from "./store";
+import { theme } from "./theme";
+import App from "./App";
 
-export default function Admin() {
-	return (
-		<Provider store={store}>
-			<App />
-		</Provider>
-	);
-}
+ReactDOM.createRoot(document.getElementById("_oratechies-root")).render(
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </Provider>
+);
